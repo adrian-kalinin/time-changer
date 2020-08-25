0d9715424113ee777c2f8bb4ed3eb9739b2f707b
Adrian Kalinin <40829881+adreex@users.noreply.github.com>
2020-08-25 21:27:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions.photos import UploadProfilePhotoRequest, DeletePhotosRequest
-from utils import *
-import config
+from telethon.tl.functions.account import UpdateProfileRequest
 import time
+
+from utils import generate_image, delete_image, get_current_time, get_progress_of_the_day
+import config
 
 
 def main():
     previous_time = ''
+    previous_progress_of_the_day = ''
+
     with TelegramClient(config.session_name, config.api_id, config.api_hash) as client:
         while True:
             if not previous_time == get_current_time():
                 current_time = get_current_time()
                 previous_time = current_time
                 generate_image(current_time)
-                image = client.upload_file('time_image.jpg')
-                client(DeletePhotosRequest(client.get_profile_photos('me')))
+                image = client.upload_file(config.image_filename)
                 client(UploadProfilePhotoRequest(image))
+                client(DeletePhotosRequest([client.get_profile_photos('me')[-1]]))
+                delete_image()
                 time.sleep(1)
 
+            if not previous_progress_of_the_day == get_progress_of_the_day():
+                current_progress_of_the_day = get_progress_of_the_day()
+                previous_progress_of_the_day = current_progress_of_the_day
+                profile_bio = config.profile_bio.format(current_progress_of_the_day)
+                client(UpdateProfileRequest(about=profile_bio))
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 from PIL import Image, ImageFont, ImageDraw
 from datetime import datetime
 from pytz import timezone
+import os
+
+import config
 
 
 def get_current_time():
@@ -8,12 +11,32 @@ def get_current_time():
 
 
 def generate_image(text):
-    image = Image.new('RGB', (500, 500), color='black')
+    image = Image.open(os.getcwd() + config.photo_filename)
     W, H = image.size
     draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype(font='resources/ds-digit.ttf', size=212)
+    font = ImageFont.truetype(font='resources/ds-digit.ttf', size=270)
     wt, ht = draw.textsize(text, font=font)
-    draw.text(((W - wt) / 2, (H - ht) / 2 ), text, font=font, fill='#157381')
 
-    image.save('time_image.jpg')
+    border_size = config.image_border_size
+    border_color = config.image_border_color
+
+    draw.text(((W - wt) / 2 - border_size, (H - ht) / 2 - border_size), text, font=font, fill=border_color)
+    draw.text(((W - wt) / 2 + border_size, (H - ht) / 2 - border_size), text, font=font, fill=border_color)
+    draw.text(((W - wt) / 2 - border_size, (H - ht) / 2 + border_size), text, font=font, fill=border_color)
+    draw.text(((W - wt) / 2 + border_size, (H - ht) / 2 + border_size), text, font=font, fill=border_color)
+
+    draw.text(((W - wt) / 2, (H - ht) / 2), text, font=font, fill=config.image_text_color)
+
+    image.save(config.image_filename)
+
+
+def delete_image():
+    os.remove(config.image_filename)
+
+
+def get_progress_of_the_day():
+    now = datetime.now(timezone('Europe/Moscow'))
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    seconds = (now - midnight).seconds
+    return round(seconds / 86400 * 100, 1)

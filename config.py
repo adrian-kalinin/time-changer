d114d1cd6874b63d6d56ebeb95c5802d885a7089
Adrian Kalinin <40829881+adreex@users.noreply.github.com>
2020-08-25 21:28:34 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 api_id = 0
 api_hash = ''
 session_name = 'session'
+
+image_filename = 'time_image.jpg'
+photo_filename = '/resources/photo.png'
+image_text_color = 'white' # '#ff0800'
+image_border_color = 'black'
+image_border_size = 3
+
+profile_bio = 'Прогресс дня: {}%'
